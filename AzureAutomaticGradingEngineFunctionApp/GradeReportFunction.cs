db76505c91b34030da12748e9352d341214b5628
cyrus wong <cywong@vtc.edu.hk>
2021-11-02 13:26:52 +0800

    Add SaveTodayMarkJson Activity to resolve SaveMarkJson 10 minutes timeout problem. Add Time sheet in report to show the task complete time for each student as accumulated mark cannot reflect who completes the task first especially after student subscription has used up.

--- AzureAutomaticGradingEngineFunctionApp/GradeReportFunction.cs
+++ AzureAutomaticGradingEngineFunctionApp/GradeReportFunction.cs
@@ -22,7 +22,7 @@ namespace AzureAutomaticGradingEngineFunctionApp
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log, ExecutionContext context)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation($@"C# HTTP trigger {nameof(GradeReportFunction)} function processed a request.");
 
             string assignment = req.Query["assignment"];
             bool isJson = req.Query.ContainsKey("json");
@@ -48,15 +48,15 @@ namespace AzureAutomaticGradingEngineFunctionApp
             }
         }
 
-        public static MemoryStream WriteWorkbookToMemoryStream(Dictionary<string, Dictionary<string, int>> accumulateMarks, MemoryStream stream)
+        public static MemoryStream WriteWorkbookToMemoryStream(Dictionary<string, (Dictionary<string, int>, Dictionary<string, DateTime>)> accumulateMarks, MemoryStream stream)
         {
             using var workbook = new XLWorkbook();
-            GenerateMarksheet(accumulateMarks, workbook);
+            GenerateMarksheets(accumulateMarks, workbook);
             workbook.SaveAs(stream);
             return stream;
         }
 
-        public static async Task<Dictionary<string, Dictionary<string, int>>> CalculateMarks(ILogger log, ExecutionContext context, string assignment, bool isToday)
+        public static async Task<Dictionary<string, (Dictionary<string, int>, Dictionary<string, DateTime>)>> CalculateMarks(ILogger log, ExecutionContext context, string assignment, bool isToday)
         {
             var storageAccount = CloudStorage.GetCloudStorageAccount(context);
             var blobClient = storageAccount.CreateCloudBlobClient();
@@ -67,38 +67,54 @@ namespace AzureAutomaticGradingEngineFunctionApp
             var testResults = blobItems.Select(c => new
             {
                 Email = ExtractEmail(c.Uri.ToString()),
-                TestResult = GetTestResult(container, c),
-                CreateTime = GetTestTime(container, c)
-            });
+                XmlDoc = LoadTestResultToXmlDocument(container, c),
+            }).Select(c => new
+            {
+                c.Email,
+                TestResult = ParseNUnitTestResult(c.XmlDoc),
+                CreateTime = GetTestTime(c.XmlDoc)
+            }).Select(c => new
+            {
+                c.Email,
+                c.TestResult,
+                TestCompeteTime = c.TestResult.Select(a => (a.Key, a.Value == 1 ? c.CreateTime : DateTime.MaxValue)).ToDictionary(d => d.Key, d => d.Item2),
+                c.CreateTime
+            }); ;
 
             bool IsNotToday(DateTime date) => (new DateTime(date.Year, date.Month, date.Day)).Subtract(new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day)).Days != 0;
 
 
-            var accumulateMarks = testResults.Aggregate(new Dictionary<string, Dictionary<string, int>>(), (acc, item) =>
+            var accumulateMarks = testResults.Aggregate(new Dictionary<string, (Dictionary<string, int>, Dictionary<string, DateTime>)>(), (acc, item) =>
             {
                 if (isToday && IsNotToday(item.CreateTime))
                 {
                     return acc; //Skip it.
                 }
-
                 if (acc.ContainsKey(item.Email))
                 {
                     var previous = acc[item.Email];
-                    var current = item.TestResult;
+                    var currentMark = item.TestResult;
                     //Key is testname and Value is list of (testname,mark)
-                    var result = previous.Concat(current).GroupBy(d => d.Key)
+                    var markResult = previous.Item1.Concat(currentMark).GroupBy(d => d.Key)
                         .ToDictionary(d => d.Key, d => d.Sum(c => c.Value));
-                    acc[item.Email] = result;
+
+                    var currentTime = item.TestCompeteTime;
+                    var timeResult = previous.Item2.Concat(currentTime).GroupBy(d => d.Key)
+                        .ToDictionary(d => d.Key, d => d.Min(c => c.Value));
+
+                    acc[item.Email] = (markResult, timeResult);
                     return acc;
                 }
-
-                acc.Add(item.Email, item.TestResult);
-                return acc;
+                else
+                {
+                    acc.Add(item.Email, (item.TestResult, item.TestCompeteTime));
+                    return acc;
+                }
             });
             return accumulateMarks;
         }
 
-        private static void GenerateMarksheet(Dictionary<string, Dictionary<string, int>> accumulateMarks, XLWorkbook workbook)
+        private static void GenerateMarksheets(Dictionary<string, (Dictionary<string, int>, Dictionary<string, DateTime>)> accumulateMarks, XLWorkbook workbook)
         {
             var worksheet =
             workbook.Worksheets.Add("Marks");
@@ -110,8 +126,8 @@ namespace AzureAutomaticGradingEngineFunctionApp
             {
                 var student = accumulateMarks.ElementAt(i);
                 worksheet.Cell(i + 2, 1).Value = student.Key;
-                tests.UnionWith(student.Value.Keys.ToHashSet());
-                worksheet.Cell(i + 2, 2).Value = student.Value.Values.Sum();
+                tests.UnionWith(student.Value.Item1.Keys.ToHashSet());
+                worksheet.Cell(i + 2, 2).Value = student.Value.Item1.Values.Sum();
             }
             var testList = tests.ToList();
             testList.Sort();
@@ -122,7 +138,41 @@ namespace AzureAutomaticGradingEngineFunctionApp
                 for (var i = 0; i < accumulateMarks.Count(); i++)
                 {
                     var student = accumulateMarks.ElementAt(i);
-                    worksheet.Cell(i + 2, j + 3).Value = student.Value.GetValueOrDefault(testName, 0);
+                    worksheet.Cell(i + 2, j + 3).Value = student.Value.Item1.GetValueOrDefault(testName, 0);
+                }
+            }
+
+            worksheet =
+                workbook.Worksheets.Add("Time");
+            worksheet.Cell(1, 1).Value = "Email";
+            worksheet.Cell(1, 2).Value = "Begin at";
+
+            tests = new HashSet<string>();
+            for (var i = 0; i < accumulateMarks.Count(); i++)
+            {
+                var student = accumulateMarks.ElementAt(i);
+                worksheet.Cell(i + 2, 1).Value = student.Key;
+                tests.UnionWith(student.Value.Item2.Keys.ToHashSet());
+                var beginDateTime = student.Value.Item2.Values.Min(c => c);
+                if (beginDateTime == DateTime.MaxValue)
+                    worksheet.Cell(i + 2, 2).Value = "";
+                else
+                    worksheet.Cell(i + 2, 2).Value = beginDateTime;
+            }
+            testList = tests.ToList();
+            testList.Sort();
+            for (var j = 0; j < testList.Count(); j++)
+            {
+                var testName = testList.ElementAt(j);
+                worksheet.Cell(1, j + 3).Value = testName;
+                for (var i = 0; i < accumulateMarks.Count(); i++)
+                {
+                    var student = accumulateMarks.ElementAt(i);
+                    var completeTime = student.Value.Item2.GetValueOrDefault(testName, DateTime.MaxValue);
+                    if (completeTime == DateTime.MaxValue)
+                        worksheet.Cell(i + 2, j + 3).Value = "";
+                    else
+                        worksheet.Cell(i + 2, j + 3).Value = completeTime;
                 }
             }
         }
@@ -171,9 +221,8 @@ namespace AzureAutomaticGradingEngineFunctionApp
             return result;
         }
 
-        private static DateTime GetTestTime(CloudBlobContainer cloudBlobContainer, IListBlobItem item)
+        private static DateTime GetTestTime(XmlDocument xmlDoc)
         {
-            var xmlDoc = LoadTestResultToXmlDocument(cloudBlobContainer, item);
             //ISO 8601 pattern "2021-10-02T10:01:57.1589935Z"
             var testStartTime = DateTime.Parse(xmlDoc.SelectSingleNode("/test-run")?.Attributes?["start-time"].Value);
             //Ignore Second.
